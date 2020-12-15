058b77c3d5329d36bcdf30906cb86e1a1fc25707
Hoc Can Huynh <canhuynh1998@gmail.com>
2020-12-14 20:45:16 -0800

    Add edit input ability

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
 
 
+//search.readOnly = false;
+
 //Search state.json and fliter
 const searchStates = async searchText =>{
 
@@ -16,10 +18,13 @@ const searchStates = async searchText =>{
     //Get matches from input
     let matches = states.filter(state =>{
         const regex = new RegExp(`${searchText}`,'gi');
+
         return state.name.match(regex) || state.abbr.match(regex);
     });
     console.log(matches);
-    
+    // $('search').
+    //$("input[type='text']").on("placeholder",matches[0].name);
+    //$('input[name="state"]').attr('placeholder', matches[0].name.text()); 
     //Make sure there are only at max 8 results
     while(matches.length > 8){
         matches.pop();
@@ -35,6 +40,7 @@ const searchStates = async searchText =>{
 
 }
 
+
 //Show result in HTML
 const outputHtml = matches =>{
     if(matches.length > 0){
@@ -42,51 +48,28 @@ const outputHtml = matches =>{
         const html = matches.map(match => `
         <div class="select-box">
         <div class ="options-container">
-        <div class ="option">
-        <input type="radio" class ="radio">
+        <div class ="option" id ="selection:>
+        <input type="radio" class ="radio" readonly ="false">
         <label>${match.name}</label>
         </div>
         </div>
         </div>
+
         `)
         .join('');
+        console.log("Hi");
         matchList.innerHTML =html;
+
         
-        $('.option').on('click', function(){
-            $('#form')[0].reset();
-            $('input[name="state"]').attr('placeholder',$(this).text());    //write the result back into the placeholder
+        $('.option').on('click', function(event){
+            search.value = ($(this).text().trim());
             console.log($(this).text());    //testing in the console.
             matches = [];   //clear the array when targeted State is clicked
             matchList.innerHTML = '';   // clear the screen
         });
 
-        // $('.option').on('click', function(){
-        //     
-        // })
-        // $('#search').text(function(e){
-        //     console.log(e.target.value);
-        // });
-        // // $('button').on('mouseenter', function(){
-        // //     $('button').toggle();
-        // // });
-
-        // // $('button').on('mouseleave', function(){
-        // //     $('button').toggle();
-        // // });
-        // $('button').on('click', function(){
-        //     $('input').attr("placeholder",$(this).text());
-        // });
     }
 }
 
 search.addEventListener('input',() => searchStates(search.value));  //event listener
-// var button = document.getElementById('currentbutton')
-// button.onclick = function(){
-//     alert('Clicked');
-// };
 
-// search.addEventListener('keydown',() => searchStates(search.value));
-// var button = document.getElementById('currentbutton')
-// button.onclick = function(){
-//     alert('Clicked');
-// };
